5611f303a5cd25673bb62f5077bb3c4327440bc7
Yoshi K <kai.outsider.777+git@gmail.com>
2020-06-25 16:01:41 +0900

    Add Submitting the Form

--- app/Link.php
+++ app/Link.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Link extends Model
 {
-    //
+    // この定義を追加することで一括割当することができる(コンストラクタで自動的に設定される)
+    protected $fillable = [
+        'title',
+        'url',
+        'description'
+    ];
+    // 一括割当を避けるなら個別に代入する。
+    // $link = new \App\Link;
+    // $link->title = $data['title'];
+    // $link->url = $data['url'];
+    // $link->description = $data['description'];
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -30,4 +31,20 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/submit', function () {
     return view('submit');
+});
+
+Route::post('/submit', function (Request $request) {
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'url' => 'required|url|max:255',
+        'description' => 'required|max:255',
+    ]);
+
+    // tap ヘルパーを使うとモデルインスタンスが返るため save を呼ぶことができる
+    $link = tap(new App\Link($data))->save();
+    // tap ヘルパーを使わないケースは下記。
+    // $link = new \App\Link($data);
+    // $link->save();
+
+    return redirect('/');
 });
